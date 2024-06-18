32f96b4d912a21581416c17c48509be41c1110f8
Oleh Volynets <saggot91@gmail.com>
2024-06-18 21:42:00 +0200

    feat: added status code loging

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -7,17 +7,48 @@ import (
 	"time"
 )
 
+type responseStatusCatcher struct {
+	w          http.ResponseWriter
+	statusCode int
+}
+
+func (c *responseStatusCatcher) Header() http.Header {
+	return c.w.Header()
+}
+
+func (c *responseStatusCatcher) Write(b []byte) (int, error) {
+	return c.w.Write(b)
+}
+
+func (c *responseStatusCatcher) WriteHeader(statusCode int) {
+	c.statusCode = statusCode
+	c.w.WriteHeader(statusCode)
+}
+
+func (c *responseStatusCatcher) StatusCode() int {
+	fmt.Println("returning a status")
+	if c.statusCode == 0 {
+		fmt.Println("status 0")
+		return http.StatusOK
+	}
+	fmt.Println("status not 0")
+
+	return c.statusCode
+}
+
 func Logger(f http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startT := time.Now()
 		// slog.Info("started", "method", r.Method, "path", r.URL.Path)
 
-		f.ServeHTTP(w, r)
+		ww := responseStatusCatcher{w: w}
+		f.ServeHTTP(&ww, r)
 
 		endT := time.Now()
 		took := endT.Sub(startT).Seconds()
 
 		slog.Info("req",
+			"status", ww.statusCode,
 			"method", r.Method,
 			"path", r.URL.Path,
 			"took", fmt.Sprintf("%.6f", took))

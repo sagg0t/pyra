4dd76aa1d0fd83d298c6ea4b8f7b4fedd3ed925c
Oleh Volynets <saggot91@gmail.com>
2025-05-14 13:30:00 +0200

    feat: upgrade tailwindcss to v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,10 @@
-import postCSSImport from "postcss-import";
-import tailwindCSS from "tailwindcss";
-import autoprefixer from "autoprefixer";
 import postCSSFlexbugsFixes from "postcss-flexbugs-fixes";
 
 /** @type {import('postcss-load-config').Config} */
 export default {
     plugins: {
-        "postcss-import": {},
-        "tailwindcss/nesting": {},
-        tailwindcss: {},
-        autoprefixer: {},
+        "@tailwindcss/postcss": {},
+        "@tailwindcss/nesting": {},
         "postcss-flexbugs-fixes": {},
     }
 }
